1bae44bfb17ef0b034afb311b46024df09431630
red1bluelost <msweston12@cox.net>
2020-04-04 22:09:36 -0700

    Initial implementation of a basic recursive merge sort

---
+++ basic_recursive_merge/basic_merge_sort_rec.go
@@ -0,0 +1,48 @@
+package basic_recursive_merge
+
+func MergeSort(arr []int) {
+	mergeSortRec(arr, 0, len(arr)-1)
+}
+
+func mergeSortRec(arr []int, l, r int) {
+	if l < r {
+		m := l + (r-l)/2
+		mergeSortRec(arr, l, m)
+		mergeSortRec(arr, m+1, r)
+		merge(arr, l, m, r)
+	}
+}
+
+func merge(arr []int, l, m, r int) {
+	lenL, lenR := m-l+1, r-m
+
+	//create temporary slices
+	arrL, arrR := make([]int, lenL), make([]int, lenR)
+	copy(arrL, arr[l:m+1])
+	copy(arrR, arr[m+1:r+1])
+
+	//merge the temporary arrays into the main slice
+	i, j, k := 0, 0, 0
+	for i < lenL && j < lenR {
+		if arrL[i] <= arrR[j] {
+			arr[k] = arrL[i]
+			i++
+		} else {
+			arr[k] = arrR[j]
+			j++
+		}
+		k++
+	}
+
+	//copy remaining elements into main slice if necessary
+	for i < lenL {
+		arr[k] = arrL[i]
+		i++
+		k++
+	}
+	for j < lenR {
+		arr[k] = arrR[j]
+		j++
+		k++
+	}
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+
+)
+
+func main() {
+	fmt.Println("Hello World")
+	slice1 := []int{8, 7, 6, 5, 4, 3, 2, 1}
+	fmt.Println("Slice1 before:", slice1)
+}
